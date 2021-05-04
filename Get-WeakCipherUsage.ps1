39d8c5387a97b058409486264a600798c147aa45
1nTh35h3ll <yossis@protonmail.com>
2021-05-04 11:43:54 +0300

    Update Get-WeakCipherUsage.ps1

--- Get-WeakCipherUsage.ps1
+++ Get-WeakCipherUsage.ps1
@@ -1,6 +1,6 @@
 # Get weak cipher usage (RC4 used in Domain environments). useful for On-Prem diagnostics, similar to MDI (cloud app sec) weak cupher usage report.
-# By default, queries all Domain Controllers Security events logs (requires Event Log Readers or equivalent/DA).
-# OPTIOBAL: Can limit from a certain Time and Date (optional parameters), for shorter execution and avoid query overload in large environments/large Security Logs.
+# By default, queries all Domain Controllers' Security events logs (requires Event Log Readers or equivalent/DA).
+# OPTIONAL: Can limit from a certain Time and Date (optional parameters), for shorter execution and avoid query overload in large environments/large Security Logs.
 # OPTIONAL: If using an Event Forwarder to log 4769 (Kerberos TGS events) from all DCs - can also specify an Event Forwarding server.
 # Comments: 1nTh35h311 (yossis@protonmail.com)
 
